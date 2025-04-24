1876e01e0a66fa5b6416d32489df243bc33a730c
vinugh <01fe23bca166@kletech.ac.in>
2025-04-24 11:22:00 +0530

    save file

--- demo.java
+++ demo.java
@@ -4,7 +4,11 @@ public class demo{
     int b=20;
     int sum = a+b;
     int sub = a-b;
+    int mul = a*b;
+    int div = a/b;
     System.out.println("addition of number"+sum);
     System.out.println("subtraction of number"+sub);
+    System.out.println("multiple of number"+mul);
+    System.out.println("divition of number"+div);
     }
 }
