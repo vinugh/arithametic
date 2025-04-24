a31a19f4f1e5f9951594fbbe3dea1b3635c3c2c9
vinugh <01fe23bca166@kletech.ac.in>
2025-04-24 11:17:28 +0530

    save file

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String[] args){
+    int a=10;
+    int b=20;
+    int sum = a+b;
+    int sub = a-b;
+    System.out.println("addition of number"+sum);
+    System.out.println("subtraction of number"+sub);
+    }
+}
